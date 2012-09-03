370c6092aa95740399a7589b932ae4248e67f931
Juan Carlos Cerrada Garcia <jccerrada@telefonica.net>
2012-09-03 13:52:51 +0200

    Primera subida de pruebas

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WebshopOnline::Application.config.secret_token = '99b402f2724c38306a9087877d71930f98929c710e2923dfef97cf7d9d2eb8eb8a23c8925a633885aeb5ce64d53b94ee9d55500e3e3eb3797e35825252b511b7'
